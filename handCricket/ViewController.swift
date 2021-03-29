41fcd70569905a45c52bd1c1ac2a0e4d9a8f560b
tonywilson <tonywilson.j@ivymobility.com>
2021-03-29 11:28:38 +0530

    Initial Commit

---
+++ handCricket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handCricket
+//
+//  Created by Tonywilson Jesuraj on 29/03/21.
+//  Copyright © 2021 Tonywilson Jesuraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
